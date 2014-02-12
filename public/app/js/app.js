02634670c43da9947abbedda2bf1d640d59a2c7c
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-02-12 11:04:43 -0500

    started to transfer templates to Angular

---
+++ public/app/js/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var hannahApp = angular.module('hannahApp', [
+	'ngRoute',
+	'hannahControllers',
+	'hannahFilters',
+	'hannahServices'
+	]);
+
+hannahApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider.
+		when('/', {
+			templateUrl: 'partials/welcome-page.html',
+			controller: 'HannahListCtrl'
+		}).
+		when('/about',{
+			templateUrl: 'partials/my-work.html',
+			controller: 'HannahListCtrl'
+		}).
+		otherwise({
+			redirectTo: '/'
+		});
+}]);
