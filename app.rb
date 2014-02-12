5405d6f22229b96f76a27368bcfc388b6e026690
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-02-12 10:07:21 -0500

    started to set things up in Angular

--- app.rb
+++ app.rb
@@ -11,11 +11,20 @@ class CreateHannah < Sinatra::Application
 	set :database, "sqlite3:///hannahsdb.db"
 
 	get '/' do
-		erb :index
+        File.read(File.join('public/app', 'index.html'))
+		# erb :index
 	end
 
-	post '/' do
-		erb :about
+    get '/about' do
+        @hannah = Hannah.all
+        @hannah.to_json
+    end
+
+	post '/about' do
+        @hannah = Hannah.create!(params)
+
+        redirect '/'
+		# erb :about
 	end
 
 end #<--This is the end of your class
