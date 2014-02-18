dfb2419c947cba93194a72bfeebf60ec7cca04a0
hannah nordgren <hannah.nordgren@flatironschool.com>
2014-02-18 10:51:18 -0500

    got buttons disabled and enabled, I think

--- public/app/js/buttons.js
+++ public/app/js/buttons.js
@@ -1,14 +1,18 @@
-//it should discern between button type clicked
-//it should disable other button when clicked
-//use value!
 
-$(document).ready(function(){
-    console.log('buttons loaded');
-	$('button').on('click', function(event){
-        event.stopPropogation();
-	if(!$(this).hasClass('spacer')){
-        console.log('hello');
-	}
 
-	});
-});
+function disable(){
+    console.log('You clicked the button.  Have a cookie.');
+    $('#clicky1').attr('disabled', 'disabled');
+    $('#clicky2').attr('disabled', 'disabled');
+}
+
+function enable(){
+    //when the user clicks on the close button, the other buttons re-enable
+    console.log('You clicked the box button.  Hooray.');
+    $('#clicky1').removeAttr('disabled', 'disabled');
+    $('#clicky2').removeAttr('disabled', 'disabled');
+
+};
+
+
+
